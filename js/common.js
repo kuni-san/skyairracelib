c0242b9e258b13e1f0a0bfaa8de03f7e6bd7e424
kuni <9292.no6@gmail.com>
2024-10-28 03:09:52 +0900

    /で前方一致

--- js/common.js
+++ js/common.js
@@ -46,16 +46,16 @@ $('.ac-tech__index').on('click', function() {//タイトル要素をクリック
 });
 
 $(function(){
-  var windowWidth = $(window).width();
-  var headerHight = 50; 
-  $('a[href^=#]').click(function() {
-    console.log('てすと１');
-  var speed = 1000;
-  var href= jQuery(this).attr("href");
-  var target = jQuery(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top-headerHight;
-  console.log('てすと２');
-  jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
-  return false;
- });
+    var windowWidth = $(window).width();
+    var headerHight = 50; 
+    $('a[href^=/]').click(function() {
+      console.log('てすと１');
+    var speed = 1000;
+    var href= jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight;
+    console.log('てすと２');
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 });
