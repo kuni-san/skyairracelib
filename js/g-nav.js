ba841fb527c230afcfbda0979ad15159c5242c3c
kuni <9292.no6@gmail.com>
2026-04-20 11:12:36 +0900

    safariでハンバーガーメニューをスクロールしたときに、メニューバーのせいで下までスクロールできない問題を解消

--- js/g-nav.js
+++ js/g-nav.js
@@ -2,6 +2,11 @@ $(".openbtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
     $('body').css('overflow', 'hidden');//開いているページのスクロールを禁止
+    //高さ計算
+    $(document).ready(function () {
+        hsize = $(window).height();　//window高さをセット
+        $(".panelactive").css("height", hsize);
+    });
 });
 
 $("#g-nav a, main").click(function () {//ナビゲーションのリンクもしくはmainタグ内要素がクリックされたら
