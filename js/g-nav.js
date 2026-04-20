a41bfebf4f8ce121c744c2d3d865a8b11d39134f
kuni <9292.no6@gmail.com>
2026-04-20 10:12:44 +0900

    ハンバーガーメニューを開いている間後ろのページのスクロールを禁止

--- js/g-nav.js
+++ js/g-nav.js
@@ -1,9 +1,11 @@
 $(".openbtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $('body').css('overflow', 'hidden');//開いているページのスクロールを禁止
 });
 
 $("#g-nav a, main").click(function () {//ナビゲーションのリンクもしくはmainタグ内要素がクリックされたら
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+    $('body').css('overflow', 'initial');//開いているページのスクロールを禁止
 });
